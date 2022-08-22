cac2f0798b70c991c6cad9f0fc103694334dfe3b
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    Further clsssification of arhitecture
    Former-commit-id: f7165f4b31c243461b5f811080cb772aabf61294 [formerly 1b0fb01a692659cae04ff81b9992a9e8144103ef]
    Former-commit-id: 3a9a190dd61dda28ab879096ada6b23c9831d096 [formerly 99903cdcdcc33e825285fe7d7af36136327360c3] [formerly 1f7c8d2ce14d663d042f446fae98ec6a88cd9b7f [formerly 73adfd2636c8f2663524cbacfafc655f5701b95d]]
    
    Former-commit-id: ee9843ac175ba609df7ae3cf0233ad969900bb12 [formerly a95b4a9f847567dc48dc175a8294daf0088b8299]
    Former-commit-id: cd66963f7e16345f8164b5cc707a5a907196ebd4

---
+++ src/World/systems/physics/initPhysicsWorld.js
@@ -0,0 +1,21 @@
+
+function initPhysicsWorld(Ammo) {
+    let collisionConfiguration;
+    let dispatcher;
+    let broadphase;
+    let solver;
+    let physicsWorld;
+
+    // create all physics
+    collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+    dispatcher = new Ammo.btCollisionDispatcher( collisionConfiguration );
+    broadphase = new Ammo.btDbvtBroadphase();
+    solver = new Ammo.btSequentialImpulseConstraintSolver();
+    physicsWorld = new Ammo.btDiscreteDynamicsWorld( dispatcher, broadphase, solver, collisionConfiguration );
+    physicsWorld.setGravity( new Ammo.btVector3( 0, -9.82, 0 ) );
+    physicsWorld.tick = (delta) => physicsWorld.stepSimulation(delta, 10);
+
+    return physicsWorld;
+}
+
+export { initPhysicsWorld }
