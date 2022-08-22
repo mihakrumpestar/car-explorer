fab2a74d7805843b4421c4a5f4716da015a2c902
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    Added loading screen, modeling objects are finished
    Former-commit-id: 8f686f5c833053e1335e14bedd754d773d73a56b [formerly d7b26ad6140f451176b3f97acb743e3a3f565eb5]
    Former-commit-id: 11fed8efd398520400177b4d6b90d287548f9c83 [formerly 9c3116fd962942c69071c1b66e62babc1d5d531e] [formerly 725f6043f0ea4f5bdc0a856241a1f97722678a8f [formerly 11c10f5dc4bb4ccdd0faad6ed98abcfd8f8b99d6]]
    
    Former-commit-id: c7b3478cdfd7b87c7a181fb172271ffc92122f13 [formerly 8bdbbb4ef2eb7d5b1108e474a042e2a9fe17cdae]
    Former-commit-id: 4361e1ac4f6a0f0470e4fcca035330436648b729

--- src/World/components/bird/objectHelper/objectHelper.js
+++ src/World/components/bird/objectHelper/objectHelper.js
@@ -1,17 +1,9 @@
 import glTF from "./objects/glTF.js";
-import box from "./objects/box.js";
 import generic from "./objects/generic.js";
-import cone from "./objects/cone.js";
-import cylinder from "./objects/cylinder.js";
-import sphere from "./objects/sphere.js";
 import vehicle from "./objects/vehicle.js";
 
 export default {
     generic: generic,
     glTF: glTF,
     vehicle: vehicle,
-    box: box,
-    cone: cone,
-    cylinder: cylinder,
-    sphere: sphere,
 }
