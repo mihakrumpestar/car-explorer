87ea206ff5db843a9ba938a8f58fdbd7d2e0ce82
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    Semi working, jitter on the wheels is present when on objectPropertiesNode but not when on ObjectPropertiesglTF
    Former-commit-id: 7c254515dd7f7bb957f469e61b27d0d26625b0fe [formerly 870a70f8778b3d05228b50d94a07aab934a24588]
    Former-commit-id: 6aa5c64e330515ef889c58fb467c48f14f4d23bc [formerly 95dbccc77b1f784a6e1b4f6dc8144dcb45095f6d] [formerly e3c98d0cb8ebbea08d6ccc0e781a22246176b24f [formerly ee0fd4c4e6244d29ad29db5b61adc71ed23c9113]]
    
    Former-commit-id: bee09886e3e84370ac1623b06890543e703fdec5 [formerly 68326b2ce88b951e1fb271f83338a8e205e902ce]
    Former-commit-id: 5513b6e957f8fbfbd7c724bb8e52041911e0bcb5

--- src/World/components/camera.js
+++ src/World/components/camera.js
@@ -1,18 +1,35 @@
 import { Vector3, PerspectiveCamera } from './../../../vendor/build/three/three.module.js';
 
-function createCamera() {
-  const camera = new PerspectiveCamera(
-    35, // fov = Field Of View
-    1, // aspect ratio (dummy value)
-    0.1, // near clipping plane
-    1000, // far clipping plane
-  );
+class createCamera {
 
-  // move the camera back so we can view the scene
-  camera.position.set(-10.84, 10.84, -95.1);
-  camera.lookAt(new Vector3( 1.73, -100.70, -0.85 ));
+  constructor() {
+    
+    this.camera = new PerspectiveCamera(
+      35, // fov = Field Of View
+      1, // aspect ratio (dummy value)
+      0.1, // near clipping plane
+      3000, // far clipping plane
+    );
 
-  return camera;
+    // move the camera back so we can view the scene
+    this.camera.position.set(-10.84, 10.84, -95.1);
+    this.camera.lookAt(new Vector3( 1.73, -100.70, -0.85 ));
+
+    this.vehicleMesh;
+  }
+
+  tick() {
+    if (this.vehicleMesh) {
+      this.camera.lookAt(vehicleMesh.position)
+      /*
+      chaseCamPivot.getWorldPosition(v)
+      if (v.y < 1) {
+          v.y = 1
+      }
+      */ 
+      this.camera.position.lerpVectors(this.camera.position, 1, 0.05)
+    }
+  }
 }
 
 export { createCamera };
