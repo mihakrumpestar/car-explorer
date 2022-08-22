7cece99e291ac00600b97984e28cb55c63f503dc
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    Started adding assets
    Former-commit-id: e67ff23ab48e0a5d91bd97ca024346510e229252 [formerly 098656a2a543d1d2b0cd0ad5c0529512d9fac014]
    Former-commit-id: 9456756f0ca81b6299990f90f69f6af5005f1e6c [formerly 3d65e92f5de722e1668d20e68598fd4bbe69cc0f] [formerly ceb0464cd93fd896e8d3a00b761652610bbf3da2 [formerly ae9d45c93f80c2c8fac69e05f9f5a6437d2883bc]]
    
    Former-commit-id: a51123823094162f82630dd2c01798516cb042f8 [formerly ea63c78fb2c7d99309b5b57d42d26df9e3c6e786]
    Former-commit-id: 9f5474b0746339dd42c804dd3cf402bb36e591c3

--- src/World/components/bird/setupModel.js
+++ src/World/components/bird/setupModel.js
@@ -1,12 +1,12 @@
 import { glTFPhysicsDefault } from "./glTFPhysicsDefault.js";
 import { glTFLoader } from "./glTFLoader.js";
 
-async function setupModel(Ammo, path, mass, position, rotation, scale, movement) {
+async function setupModel(Ammo, path, mass, friction, position, rotation, scale, movement) {
   let data = await glTFLoader(path);
 
   console.log('Loaded new glTF model: ', data);
 
-  let [mesh, body] = await glTFPhysicsDefault(Ammo, data, mass, position, rotation, scale, movement);
+  let [mesh, body] = await glTFPhysicsDefault(Ammo, data, mass, friction, position, rotation, scale, movement);
 
   //console.log({mesh});
   //console.log({body});
