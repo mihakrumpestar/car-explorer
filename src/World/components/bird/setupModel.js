df9eb20b627f1bac0aa9ce08d9dc4ab0bf63b2a3
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    Not working glTF
    Former-commit-id: 39f1d139249e205011f7402bf404a1c415930349 [formerly 70a20130ca13d9415ff18b7439842b365b7bad82]
    Former-commit-id: 0a3d809e3c720282d9f84d41846de631ca8cf399 [formerly e5e8a16ab4b2d897d920765108b85dd32c5b67ee] [formerly a84146bf3363d03ae155eed1f2bfb5b0915dfe21 [formerly 207b5f2a440a10d884e82311d239ab1abb1d1795]]
    
    Former-commit-id: 5c4cb1f75f7ad04f3e940c93bad89e7635717a95 [formerly a7725cd7f63fa6ee2279ca80e76f00201b7300a5]
    Former-commit-id: 996afc6187c4e758b54745fefd12271cde7e0214

--- src/World/components/bird/setupModel.js
+++ src/World/components/bird/setupModel.js
@@ -6,10 +6,10 @@ async function setupModel(Ammo, path, mass, friction, position, rotation, scale,
 
   console.log('Loaded new glTF model: ', data);
 
-  let [mesh, body] = await glTFPhysicsDefault(Ammo, data, mass, friction, position, rotation, scale, movement);
+  let [mesh, body] = await glTFPhysicsDefault(Ammo, data, JSON.parse(JSON.stringify(mass)), JSON.parse(JSON.stringify(friction)), JSON.parse(JSON.stringify(position)), JSON.parse(JSON.stringify(rotation)), JSON.parse(JSON.stringify(scale)), JSON.parse(JSON.stringify(movement)));
 
-  //console.log({mesh});
-  //console.log({body});
+  console.log({mesh});
+  console.log({body});
 
   return [mesh, body];
 }
