5918dce7e5c693a904978f1ee3fa0c5fbb269c08
mihakrumpestar <miha.krumpestar@runbox.com>
2022-08-22 08:42:59 +0200

    created model object class
    Former-commit-id: b42c2d604c128ef54d8624e70866916de7c9507a [formerly bd81d474a6cf263f133b80fb43969e7e20b591f6]
    Former-commit-id: ca1fd20609e52262e4e0723c04633207a0e58154 [formerly b64e7793e2c45466452b587d22c4118b8ec34449] [formerly 30a965f1678a4fc7b7adb6fa581fd681512f32e3 [formerly d08aa38988a8f3a61029c9fbcc3d16cc3a6b95f5]]
    
    Former-commit-id: ef7db2f2891e9bc51bb75fb4e3675d577c6dd7c6 [formerly d34e2897cdb6270b6c3cfc42a9f8edb98783d506]
    Former-commit-id: 396094bc67d8a985522336ab6b9d9eb047077274

--- src/World/components/bird/setupModel.js
+++ src/World/components/bird/setupModel.js
@@ -1,17 +1,14 @@
-import { glTFPhysicsDefault } from "./glTFPhysicsDefault.js";
-import { glTFLoader } from "./glTFLoader.js";
+import { objectProperties } from '../bird/objectProperties.js';
 
-async function setupModel(Ammo, path, mass, friction, position, rotation, scale, movement) {
-  let data = await glTFLoader(path);
+async function setupModel(path, objectProperties) {
 
-  console.log('Loaded new glTF model: ', data);
+  await objectProperties.loadModel(path);
+  await objectProperties.setAnimation(true);
+  await objectProperties.createBody();
+  await objectProperties.setStaticMovement();
+  await objectProperties.setTick();
 
-  let [mesh, body] = await glTFPhysicsDefault(Ammo, data, JSON.parse(JSON.stringify(mass)), JSON.parse(JSON.stringify(friction)), JSON.parse(JSON.stringify(position)), JSON.parse(JSON.stringify(rotation)), JSON.parse(JSON.stringify(scale)), JSON.parse(JSON.stringify(movement)));
-
-  console.log({mesh});
-  console.log({body});
-
-  return [mesh, body];
+  return objectProperties.export();
 }
 
 export { setupModel };
